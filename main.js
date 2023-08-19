f632877e631c6f11da6ddc7b4b898c4e1d4ea5a7
sawasick <50393229+sawasick@users.noreply.github.com>
2023-08-19 19:00:55 +0900

    update:ver1.1

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 // ==UserScript==
 // @name         TampermonkeyDev
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.1
 // @description  tampermonkeyスクリプトを開発してる時に対象のコードを追加する
 // @author       sawasick
 // @match        http*://*/*
-// @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @grant        none
 // ==/UserScript==
 
+/*
+ver 1.1
+jQueryを使わない実装に変更
+---
+*/
+
 (function () {
-  'use strict';
+  "use strict";
   // VSCodeのLiveServerでローカルホストのポート5500を起動する
 
   const targetFileName = "main.js";
 
-  $("body").append(`<script src="http://127.0.0.1:5500/${targetFileName}"></script>`);
+  const $newElement = document.createElement("script");
+  $newElement.setAttribute("src", `http://127.0.0.1:5500/${targetFileName}`);
+  document.body.appendChild($newElement);
 })();
